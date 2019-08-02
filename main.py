e93e00fe85ed2e7fffbdd4372fc26119e9ee4152
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-08-02 12:19:00 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Author: GuillaumeOj
+
+Context: OpenClassrooms course
+https://openclassrooms.com/fr/courses/235344-apprenez-a-programmer-en-python
+
+Version: 2019.07
+
+This program is a game for playing roulette. Rules or not excalyt the real rules for simplfy the program.
+
+Rules:
+1. The player bet on a number between 0 and 49
+2. Even numbers or black and odd numbers or red.
+3. If the number is the one bet by the player, he win 3 time the bet
+4. If the number is the same color bet by the plauer, he win 50% of the bet
+"""
+
+if __name__ == '__main__':
+	main()
