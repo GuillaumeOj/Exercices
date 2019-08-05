cb15c27de27254a2c5e11fe7d1a8248f7141ba5c
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2019-08-05 16:30:20 +0200

    Merging each exercices in one repository for my mentor

---
+++ LeapYear/main.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Author: GuillaumeOj
+
+Context: OpenClassrooms course
+https://openclassrooms.com/fr/courses/235344-apprenez-a-programmer-en-python
+
+Version: 2019.07
+
+This program tell user if a year is a leap year
+"""
+
+def main():
+    """
+    Main function for interact with the user
+    """
+
+    year_confirmation = False
+
+    while year_confirmation is not True:
+        try:
+            # Asking user for a year
+            year = input("Give me a year: ")
+            year = int(year)
+
+            # This programm don't work for negative years
+            assert year >= 0
+        except ValueError:
+            print("Please give me a correct year !")
+        except AssertionError:
+            print("Please give me a year greater than or equal to zero.")
+        else:
+            year_confirmation = True
+            testing_year(year)
+
+def testing_year(year):
+    """
+    Function for testing if a year is a leap year
+    """
+
+    # Testing if the 'year' is a leap year
+    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+        message = "is a leap year."
+    else:
+        message = "is not a leap year."
+
+    print(year, message)
+
+
+if __name__ == '__main__':
+    main()
