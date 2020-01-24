b9469c15f0f40e94974665c9d87e1e7849dfbcc3
GuillaumeO <7831777+GuillaumeOj@users.noreply.github.com>
2020-01-24 18:14:32 +0100

    Update the page for the post according to the instructions

--- Blog/my_blog.sql
+++ Blog/my_blog.sql
@@ -158,7 +158,7 @@ GROUP BY Article.id
 ORDER BY Article.date_publication DESC;
 
 -- Page auteur
-SELECT Utilisateur.pseudo as 'Page de l\'utilisateur "2"'
+SELECT Utilisateur.pseudo AS 'Page de l\'utilisateur "2"'
 FROM Utilisateur
 WHERE Utilisateur.id = 2;
 
@@ -174,7 +174,7 @@ WHERE Article.auteur_id = 2
 ORDER BY Article.date_publication DESC;
 
 -- Page catégorie
-SELECT Categorie.nom as 'Page de la catégorie "3"'
+SELECT Categorie.nom AS 'Page de la catégorie "3"'
 FROM Categorie
 WHERE Categorie.id = 3;
 
@@ -190,22 +190,25 @@ WHERE Categorie_article.categorie_id = 3
 ORDER BY Article.date_publication DESC;
 
 -- Page de l'article
-SELECT Article.titre as 'Page de l\'article "2"'
+SELECT Article.titre AS 'Page de l\'article "4"'
 FROM Article
-WHERE Article.id = 2;
+WHERE Article.id = 4;
 
-SELECT 
-    Article.titre,
-    Article.date_publication,
-    Article.contenu,
-    Utilisateur.pseudo AS nom_auteur_article
+SELECT
+    DATE_FORMAT(Article.date_publication, '%d %M \'%y à %k heures %i') AS date_publication,
+    Article.titre AS titre_article,
+    Article.contenu AS texte_article,
+    Utilisateur.pseudo AS auteur_article,
+    Categorie.nom AS nom_categorie
 FROM Article
 JOIN Utilisateur ON Utilisateur.id = Article.auteur_id
-WHERE Article.id = 2;
+JOIN Categorie_article ON Categorie_article.article_id = Article.id
+JOIN Categorie ON Categorie.id = Categorie_article.categorie_id
+WHERE Article.id = 4;
 
 SELECT 
-    Commentaire.contenu,
-    DATE_FORMAT(Commentaire.date_commentaire, '%d/%m/%Y'),
+    Commentaire.contenu AS commentaire,
+    DATE_FORMAT(Commentaire.date_commentaire, '%d/%m/%Y') AS date_commentaire,
     Utilisateur.pseudo AS nom_auteur_commentaire
 FROM Commentaire
 JOIN Utilisateur ON Utilisateur.id = Commentaire.auteur_id
